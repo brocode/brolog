7cad5cb6258f7dbd364267e5703e2cdbc0eb9511
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2021-12-20 22:09:19 +0100

    [chore] default all values to false

--- src/main/kotlin/sh/brocode/brolog/LogLevel.kt
+++ src/main/kotlin/sh/brocode/brolog/LogLevel.kt
@@ -1,11 +1,11 @@
 package sh.brocode.brolog
 
 enum class LogLevel(
-    val traceEnabled: Boolean,
-    val debugEnabled: Boolean,
-    val infoEnabled: Boolean,
-    val warnEnabled: Boolean,
-    val errorEnabled: Boolean,
+    val traceEnabled: Boolean = false,
+    val debugEnabled: Boolean = false,
+    val infoEnabled: Boolean = false,
+    val warnEnabled: Boolean = false,
+    val errorEnabled: Boolean = false,
 ) {
     TRACE(
         traceEnabled = true,
@@ -15,31 +15,21 @@ enum class LogLevel(
         errorEnabled = true,
     ),
     DEBUG(
-        traceEnabled = false,
         debugEnabled = true,
         infoEnabled = true,
         warnEnabled = true,
         errorEnabled = true,
     ),
     INFO(
-        traceEnabled = false,
-        debugEnabled = false,
         infoEnabled = true,
         warnEnabled = true,
         errorEnabled = true,
     ),
     WARN(
-        traceEnabled = false,
-        debugEnabled = false,
-        infoEnabled = false,
         warnEnabled = true,
         errorEnabled = true,
     ),
     ERROR(
-        traceEnabled = false,
-        debugEnabled = false,
-        infoEnabled = false,
-        warnEnabled = false,
         errorEnabled = true,
     ),
 }

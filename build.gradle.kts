f4828187af865aa683c19e4abf7e70d6c1376503
Patrick Haun <patrick@bomgar.email>
2022-01-07 08:55:05 +0100

    [build] add nexus publishing plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     id("com.diffplug.spotless") version "6.0.0"
     `maven-publish`
     signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 repositories {
@@ -87,4 +88,13 @@ publishing {
 
 signing {
     sign(publishing.publications["mavenJava"])
+}
+
+nexusPublishing {
+    repositories {
+        sonatype { // only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
 }

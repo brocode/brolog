a8f5d4f3960989deb631a48908be2da609726642
Patrick Haun <patrick@bomgar.email>
2022-10-03 10:33:33 +0200

    bump kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ group = "sh.brocode"
 version = "0.4-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "1.7.10"
-    kotlin("plugin.serialization") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.20"
     `java-library`
     id("com.diffplug.spotless") version "6.11.0"
     `maven-publish`
